e9e44d16149a34a7abaa1472234d89fe87c18ed8
dante <daniellewinski95@gmail.com>
2017-04-06 17:49:16 +0200

    Removed methods' bodies

--- objects/agent.cpp
+++ objects/agent.cpp
@@ -50,105 +50,42 @@ void Agent::Render(SDL_Renderer* renderer)
 
 Vector2 Agent::Seek(Vector2 target) const
 {
-	Vector2 desiredVelocity = (target - position).Normalize() * maxSpeed;
-	Vector2 steering = desiredVelocity - velocity;
-	steering.Limit(maxForce);
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Flee(Vector2 target) const
 {
-	Vector2 desiredVelocity = (position - target).Normalize() * maxSpeed;
-	Vector2 steering = desiredVelocity - velocity;
-	steering.Limit(maxForce);
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Arrive(Vector2 target, const double slowingRadius) const
 {
-	Vector2 desiredVelocity = target - position;
-	double distance = desiredVelocity.Length();
-	
-	if(distance < slowingRadius)
-	{
-		desiredVelocity = desiredVelocity.Normalize() * maxSpeed * (distance / slowingRadius);
-	}
-	else
-	{
-		desiredVelocity = desiredVelocity.Normalize() * maxSpeed;
-	}
-	
-	Vector2 steering = desiredVelocity - velocity;
-	steering.Limit(maxForce);
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Wander(double circleDistance, double circleRadius) const
 {
-	Vector2 center = velocity;
-	center.Normalize();
-	center *= circleDistance;
-	
-	Vector2 displacement = velocity;
-	displacement.Normalize();
-	displacement *= circleRadius;
-	
-	double angle = rand()%360 - 180;
-	
-	displacement = displacement.Rotate(angle);
-	
-	Vector2 steering = displacement + center;
-	steering.Limit(maxForce);
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Pursue(Agent& agent) const
 {
-	int expectedTime = (position - agent.GetPosition()).Length() / agent.GetMaxSpeed();
-	Vector2 futurePosition = agent.GetPosition() + agent.GetVelocity() * expectedTime;
-	return Seek(futurePosition);
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Evade(Agent& agent) const
 {
-	int expectedTime = (position - agent.GetPosition()).Length() / agent.GetMaxSpeed();
-	Vector2 futurePosition = agent.GetPosition() + agent.GetVelocity() * expectedTime;
-	return Flee(futurePosition);
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Follow(Agent& agent, double distance, double arrivalRadius) const
 {	
-	const double sightRadius = agent.GetSize().x;
-	Vector2 displacement = -agent.GetVelocity();
-	displacement.Normalize();
-	displacement *= distance;
-	Vector2 behindLeader = agent.GetPosition() + displacement;
-	Vector2 aheadLeader = agent.GetPosition() + displacement*(-1);
-	
-	Vector2 steering{0,0};
-	if( (agent.GetPosition() - position).Length() < sightRadius ||  (aheadLeader - position).Length() < sightRadius )
-	{
-		steering += Evade(agent);
-	}
-	
-	steering += Arrive(behindLeader, arrivalRadius);
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::Avoid(Agent& agent, double distance, double maxAvoidForce) const
 {
-	Vector2 displacement = velocity;
-	displacement.Normalize();
-	displacement *= distance;
-	
-	Vector2 ahead = position + displacement;
-	Vector2 steering{0,0};
-
-	if( pow(ahead.x - agent.GetPosition().x, 2) + pow(ahead.y - agent.GetPosition().y, 2) < pow(agent.GetSize().x/2,2))
-	{
-		steering = (ahead - agent.GetPosition()) * maxAvoidForce;
-	}
-	return steering;
+	return Vector2{0,0};
 }
 
 Vector2 Agent::GetVelocity() const { return velocity; } 
